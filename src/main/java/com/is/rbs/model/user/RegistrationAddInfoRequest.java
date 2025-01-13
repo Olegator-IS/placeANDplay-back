29e6015534affcc392a9bd5db46c8fe24b9fad02
oleg <abramov.o.o.1998@gmail.com>
2025-01-13 18:19:05 +0500

    Добавлена вторичная регистрация для польщователей

---
+++ src/main/java/com/is/rbs/model/user/RegistrationAddInfoRequest.java
@@ -0,0 +1,38 @@
+package com.is.rbs.model.user;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@ApiOperation(value = "Вторичная регистрация пользователя", notes = "Передаётся JSON модель, содержащая userId, hobbies, favoriteSports, currentLocationCityId, currentLocationCountryId, bio, profilePictureUrl")
+public class RegistrationAddInfoRequest {
+
+    @ApiModelProperty(value = "Идентификатор пользователя", example = "1")
+    private Long userId;
+
+    @ApiModelProperty(value = "Хобби пользователя", example = "Reading, Swimming")
+    private String hobbies;
+
+    @ApiModelProperty(value = "Избранные виды спорта пользователя", example = "[{\"sportId\": 1, \"skillId\": 5, \"readyToTeach\": true}, ...]")
+    private List<FavoriteSport> favoriteSports;
+
+    @ApiModelProperty(value = "Идентификатор города текущего местоположения", example = "1")
+    private Integer currentLocationCityId;
+
+    @ApiModelProperty(value = "Идентификатор страны текущего местоположения", example = "1")
+    private Integer currentLocationCountryId;
+
+    @ApiModelProperty(value = "Биография пользователя", example = "I'm Olegator")
+    private String bio;
+
+    @ApiModelProperty(value = "URL профильной фотографии пользователя", example = "null")
+    private String profilePictureUrl;
+
+    // Конструктор, если необходим
+    public RegistrationAddInfoRequest() {}
+}
--- src/main/java/com/is/rbs/repository/UserRepository.java
+++ src/main/java/com/is/rbs/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.is.rbs.repository;
 
 import com.is.rbs.model.user.User;
+import com.is.rbs.model.user.UserAdditionalInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
